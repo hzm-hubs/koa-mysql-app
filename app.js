ea396aee37c02195c004e10b5b9a4accaa4b02d9
hzm <467251648@qq.com>
2025-08-17 20:13:21 +0800

    切换到koa-body插件

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const { exec } = require("node:child_process");
 const cors = require("koa2-cors");
 
 // POST请求参数需要用到
-const bodyParser = require("koa-body-parser");
+const { koaBody } = require("koa-body");
 
 const app = new koa();
 
-app.use(bodyParser());
+app.use(koaBody());
 
 /**
  *  可以设置代理
@@ -26,60 +26,60 @@ app.use(bodyParser());
  */
 
 app.use(
-	cors({
-		origin: function (ctx) {
-			return "*";
-		},
-		// exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
-		maxAge: 5,
-		// credentials: true,
-		// allowMethods: ['GET', 'POST', 'DELETE', 'PUT'],
-		// allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
-	})
+  cors({
+    origin: function (ctx) {
+      return "*";
+    },
+    // exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    maxAge: 5,
+    // credentials: true,
+    // allowMethods: ['GET', 'POST', 'DELETE', 'PUT'],
+    // allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  })
 );
 
 // middleware1
 app.use(async (ctx, next) => {
-	/**
-	 *  当一个中间件调用 next() 则该函数暂停并将控制传递给定义的下一个中间件。
-	 *  当在下游没有更多的中间件执行后，堆栈将展开并且每个中间件恢复执行其上游行为。
-	 */
-	// 执行权先交给下一个中间件
-	await next();
+  /**
+   *  当一个中间件调用 next() 则该函数暂停并将控制传递给定义的下一个中间件。
+   *  当在下游没有更多的中间件执行后，堆栈将展开并且每个中间件恢复执行其上游行为。
+   */
+  // 执行权先交给下一个中间件
+  await next();
 
-	// 获取来自下一个中间件的结果
-	let rt = ctx.response.get("X-Response-Time");
-	let ts = new Date().toLocaleString();
+  // 获取来自下一个中间件的结果
+  let rt = ctx.response.get("X-Response-Time");
+  let ts = new Date().toLocaleString();
 
-	let curLog = `${ctx.method} ${ctx.url} - ${rt}, ${ts}`;
+  let curLog = `${ctx.method} ${ctx.url} - ${rt}, ${ts}`;
 
-	// 控制台输出日志
-	console.log(curLog);
-	// 或者新开终端 tail -f ./logs/index.log 循环打印日志
+  // 控制台输出日志
+  console.log(curLog);
+  // 或者新开终端 tail -f ./logs/index.log 循环打印日志
 
-	exec(
-		`echo ${curLog} >> ${path.join(__dirname, "./logs/index.log")}`,
-		(error, stdout, stderr) => {
-			if (error) {
-				console.error(`exec error: ${error}`);
-				return;
-			}
-			// console.log(`stdout: ${stdout}`);
-			// console.error(`stderr: ${stderr}`);
-		}
-	);
+  exec(
+    `echo ${curLog} >> ${path.join(__dirname, "./logs/index.log")}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      // console.log(`stdout: ${stdout}`);
+      // console.error(`stderr: ${stderr}`);
+    }
+  );
 });
 
 // middleware2
 app.use(async (ctx, next) => {
-	const start = Date.now();
+  const start = Date.now();
 
-	await next();
+  await next();
 
-	const ms = Date.now() - start;
+  const ms = Date.now() - start;
 
-	// 上下文设置 X-Response-Time 属性，可供上一层中间件调用
-	ctx.set("X-Response-Time", `${ms}.ms`);
+  // 上下文设置 X-Response-Time 属性，可供上一层中间件调用
+  ctx.set("X-Response-Time", `${ms}.ms`);
 });
 
 module.exports = app;
