c269a2a34b9a52b8afffd1e16f360c19064f2196
hzm <467251648@qq.com>
2025-09-07 15:04:56 +0800

    暂提光合验证

--- router/playwright/guanghe.js
+++ router/playwright/guanghe.js
@@ -2,55 +2,97 @@
 const { chromium } = require("playwright");
 const fs = require("fs");
 const path = require("path");
+const downloadPath = path.join(
+  __dirname,
+  "/downloads/storage/guanghe-auth.json"
+);
+
+const downloadImagePath = path.join(__dirname, "/downloads/images/guanghe");
+
 (async () => {
-	// 启动浏览器实例
-	const browser = await chromium.launch({ headless: false });
+  // 启动浏览器实例
+  const browser = await chromium.launch({ headless: false });
 
-	// 模拟window
-	const context = await browser.newContext({
-		// 禁用所有扩展
-		// bypassCSP: true, // 绕过内容安全策略
-		// ignoreHTTPSErrors: true, // 忽略HTTPS错误
-		// // 设置更真实的用户代理
-		// userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
-	});
+  // 使用 storageState 创建 context
 
-	// 创建新页面
-	const page = await browser.newPage();
+  let context;
+  if (fs.existsSync(downloadPath)) {
+    console.log("发现 auth.json，尝试自动登录");
+    context = await browser.newContext({
+      storageState: downloadPath,
+    });
+  } else {
+    context = await browser.newContext();
+  }
 
-	// 导航到广告创意页面
-	await page.goto(
-		"https://creator.guanghe.taobao.com/page/unify/asset-overview"
-	);
+  // 用 context 创建页面
+  const page = await context.newPage();
 
-	await page.evaluate(() => {
-		document.cookie =
-			"xsecappid=aurora-shell; a1=198db7e77b44nk1grxb69j0yaaja5rii4bsbpqn7z50000223307; webId=93b11e87644d54cd7050dcfa553c0cb2; websectiga=88%3B6be45f388a1ee7bf611a69f3e174cae48f1ea02c0f8ec3256031b8be9c7ee; sec_poison_id=4f9eefd3-9b42-442f-9221-8c71c43f7f6b; ares.beaker.session.id=1756029152052035675365; access-token-ad.xiaohongshu.com=customer.leona.AT-68c517542087775411865980m18vx81rarhiu0sj; loadts=1756029158642; gid=yjYfDWdYK04DyjYfDWdWWDAID44ITykFCDKjMWuV8v00VD2802F33U888JJqq8W8WqSW0K2Y";
-		window.location.reload();
-	});
+  // 导航到广告创意页面
+  await page.goto(
+    "https://creator.guanghe.taobao.com/page/unify/asset-overview"
+  );
 
-	//   await page.waitForSelector(".login-btn-modal");
+  const isLogin = await judgeLogin(page);
 
-	//   await page.click("text=账号登录");
+  console.log("当前页面", page.url());
 
-	//   await page.fill('[name="email"]', "2253051861@qq.com");
+  // 检查是否已登录（比如页面是否跳转到 aurora）
+  if (isLogin) {
+    // 未登录，执行登录流程 会遇到滑块验证码
+    await page.waitForSelector(".login-content");
 
-	//   await page.fill('[name="password"]', "cqxhs123@");
+    await page.screenshot({ path: downloadImagePath });
 
-	//   await page.click(".cursor-pointer");
+    await page.fill('[name="fm-login-id"]', "15882542241");
 
-	//   // 点击登录并等待导航
-	//   await Promise.all([
-	//     page.waitForURL("**/aurora/**"),
-	//     page.click(".beer-login-btn"),
-	//   ]);
+    await page.fill('[name="fm-login-password"]', "hzm888");
 
-	console.log("登录成功，检测并处理弹窗...");
+    await page.click(".fm-agreement");
 
-	// 获取出来是空的
-	// const cookie1 = await context.cookies();
+    // 点击登录并等待导航
+    await Promise.all([
+      page.waitForURL("**/page/**"),
+      page.click(".password-login"),
+    ]);
 
-	const cookie2 = await page.evaluate(() => document.cookie);
+    console.log("已完成登录流程");
 
-	console.log("cookie2", cookie2);
+    // 登录后保存状态
+    await context.storageState({
+      path: downloadPath,
+    });
+  } else {
+    console.log("已自动登录，无需再次登录");
+  }
+
+  // 获取页面标题
+  const title = await page.title();
+
+  console.log(`页面标题: ${title}`);
+
+  // 获取并打印 cookie
+  //   const cookies = await context.cookies(); // 现在又能获取了
+
+  //   console.log("context获取cookie:", cookies);
+
+  // 后续操作...
+
+  // 处理可能出现的弹窗
+  // await handlePotentialPopups(page);
+
+  // 等待并点击下载按钮
+  // await clickDownloadButton(page);
 })();
+
+async function judgeLogin(page) {
+  let isLogin = false;
+  try {
+    await page.waitForSelector("text=登录", { timeout: 5000 });
+    isLogin = true; // 找到登录按钮，说明未登录
+  } catch (e) {
+    isLogin = false; // 没找到登录按钮，说明已登录
+  }
+  console.log("是否需要登录：", isLogin);
+  return isLogin;
+}
